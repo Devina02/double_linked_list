f35b059df4ef5410622a33e91010928e64cc76b9
Devina02 <devina.fitriapngs@gmail.com>
2022-11-18 16:53:00 +0700

    Membuat public int, string dan node

--- Program.cs
+++ Program.cs
@@ -2,10 +2,17 @@
 
 namespace double_linked_list
 {
-    class Program
+    class Node
     {
-        static void Main(string[] args)
-        {
-        }
+        /* Node class represent the node of the doubly linked list
+         * it consists of the information part and links to
+         * its succeding and preceeding
+         * in terms of next and previous */
+        public int noMhs;
+        public string name;
+        //point to the succeding node
+        public Node next;
+        //pointto the preceeding node
+        public Node prev;
     }
 }
